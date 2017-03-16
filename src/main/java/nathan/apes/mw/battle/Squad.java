ff083b39dae7ca9e70cc617b763e024b8bae59fe
NathanApes <npweberftw@gmail.com>
2017-03-16 18:24:26 +0000

    Main source directory.

---
+++ src/main/java/nathan/apes/mw/battle/Squad.java
@@ -0,0 +1,134 @@
+package nathan.apes.mw.battle;
+
+import nathan.apes.mw.main.MobWars;
+import nathan.apes.mw.event.battle.*;
+
+import java.util.*;
+
+import org.bukkit.*;
+import org.bukkit.entity.*;
+import org.bukkit.event.inventory.*;
+import org.bukkit.inventory.*;
+import org.bukkit.inventory.meta.*;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Squad {
+    
+    public EntityType squadclass;
+    
+    public ArrayList<LivingEntity> squadentities = new ArrayList<LivingEntity>();
+    
+    public Player squadowner;
+    
+    public int squadindex = -1;
+    
+    public Squad(EntityType squadtype, Player owner, Location spawnloc, int index){
+        
+        squadclass = squadtype;
+        
+        squadowner = owner;
+        
+        squadindex = index;
+        
+        JavaPlugin mainclass = JavaPlugin.getProvidingPlugin(MobWars.class);
+        
+        spawnSquad(squadclass, squadowner, squadindex, spawnloc);
+        
+        openSquadGUI(squadowner, squadindex);
+        
+        mainclass.getServer().getPluginManager().registerEvents(new EventClickFunctionGUI(), mainclass);
+        
+    }
+    
+    public void spawnSquad(EntityType spawntype, Player owner, int index, Location loc){
+        
+        World bw = Bukkit.getWorld("mw_BattleWorld");
+        //Change to config later...
+        
+        Location placementloc = null;
+        
+        Squad sq = getSquadPlayer(owner, index);
+        
+        float yaw = owner.getLocation().getYaw();
+        
+        for(int i = 0; i < 5; i++){
+        
+            if(yaw > 0 && yaw < 90){ placementloc = new Location(bw, loc.getX() + (double) i, loc.getY(), loc.getZ()); }
+            if(yaw > 90 && yaw < 180){ placementloc = new Location(bw, loc.getX(), loc.getY(), loc.getZ() + (double) i); }
+            if(yaw > 180 && yaw < 270){ placementloc = new Location(bw, loc.getX() - (double) i, loc.getY(), loc.getZ()); }
+            if(yaw > 270 && yaw < 360){ placementloc = new Location(bw, loc.getX(), loc.getY(), loc.getZ() - (double) i); }
+            
+            sq.squadentities.add((LivingEntity) bw.spawnEntity(placementloc, spawntype));
+            
+            LivingEntity e = sq.squadentities.get(i); 
+            
+            e.setAI(false);
+            e.setSilent(true);
+            
+            EntityEquipment armor = e.getEquipment();
+            
+            if(spawntype == EntityType.ZOMBIE){
+                
+                armor.setHelmet(new ItemStack(Material.IRON_HELMET)); 
+                armor.setChestplate(new ItemStack(Material.IRON_CHESTPLATE));
+                armor.setLeggings(new ItemStack(Material.IRON_LEGGINGS));
+                armor.setBoots(new ItemStack(Material.IRON_BOOTS));  
+                armor.setItemInMainHand(new ItemStack(Material.IRON_SPADE));
+                
+            }
+            
+            if(spawntype == EntityType.SKELETON){
+                
+                armor.setChestplate(new ItemStack(Material.LEATHER_CHESTPLATE));
+                armor.setLeggings(new ItemStack(Material.LEATHER_LEGGINGS));
+                armor.setBoots(new ItemStack(Material.LEATHER_BOOTS));
+                armor.setItemInMainHand(new ItemStack(Material.BOW));
+                
+            }
+            
+        }
+        
+    }
+    
+    private void openSquadGUI(Player owner, int ind){
+        
+        String type = "";
+        
+        if(ind < 4){ type = "Zombie"; }
+        if(ind > 3){ type = "Skelebow"; }
+        
+        Inventory inv = Bukkit.createInventory(owner, InventoryType.CHEST, "MobSquadron #" + ind + " - " + type + " | Function?");
+        
+        ItemStack iatt = new ItemStack(Material.IRON_SWORD);
+        ItemStack igrd = new ItemStack(Material.SHIELD);
+        ItemStack isni = new ItemStack(Material.BOW); 
+        
+        ItemMeta im1 = iatt.getItemMeta();
+        im1.setDisplayName("Attack");
+        iatt.setItemMeta(im1);
+        
+        ItemMeta im2 = igrd.getItemMeta();
+        im2.setDisplayName("Guard");
+        igrd.setItemMeta(im2);
+        
+        ItemMeta im3 = isni.getItemMeta();
+        im3.setDisplayName("Snipe");
+        isni.setItemMeta(im3);
+        
+        inv.setItem(11, iatt);
+        inv.setItem(13, igrd);
+        inv.setItem(15, isni); 
+        
+        owner.openInventory(inv);
+        
+    }
+    
+    public void setAI(int typeAI, int index, Player sqowner){
+        
+        
+        
+    }
+    
+    public static Squad getSquadPlayer(Player owner, int index){ return Battle.getPlayerBattle(owner).squads.get(index); }
+    
+}
---
+++ src/main/java/nathan/apes/mw/util/PlayerQueue.java
@@ -0,0 +1,65 @@
+package nathan.apes.mw.util;
+
+import nathan.apes.mw.battle.Battle;
+import nathan.apes.mw.battle.util.UpdateBattleStage;
+import nathan.apes.mw.main.MobWars;
+import nathan.apes.mw.world.battle.*;
+
+import java.util.*;
+
+import org.bukkit.*;
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.scheduler.*;
+
+public class PlayerQueue {
+    
+    public static ArrayList<Player> playerpair = new ArrayList<Player>();
+    
+    public static ArrayList<Battle> currbattles = new ArrayList<Battle>();
+    
+    private int battleind = -1;
+    
+    public PlayerQueue(){
+        
+        BukkitScheduler scheduler = Bukkit.getScheduler();
+        
+        JavaPlugin mainclass = JavaPlugin.getProvidingPlugin(MobWars.class);
+        
+        scheduler.scheduleSyncRepeatingTask(mainclass, new Runnable(){
+            
+            public void run(){
+                
+                matchPlayer();
+                
+            }
+            
+        }, 0L, 5L);    
+        
+    }
+    
+    public void matchPlayer(){
+        
+        if(playerpair.size() == 2){
+            
+            Player p1 = playerpair.get(0);
+            Player p2 = playerpair.get(1);
+            
+            battleind++;
+            
+            if(battleind == 0){ new UpdateBattleStage(); }
+            
+            currbattles.add(new Battle(p1, p2, new FindBattleground().findBattleground(), battleind));
+            
+            p1.sendMessage("[" + ChatColor.RED + "MobWars" + ChatColor.RESET + "] " + ChatColor.GREEN + "You have an opponent! " + ChatColor.RESET + "(" + ChatColor.GOLD + p2.getPlayerListName() + ChatColor.RESET + ")" + ChatColor.GREEN + " It's now time for the battle!");
+            p2.sendMessage("[" + ChatColor.RED + "MobWars" + ChatColor.RESET + "] " + ChatColor.GREEN + "You have an opponent! " + ChatColor.RESET + "(" + ChatColor.GOLD + p1.getPlayerListName() + ChatColor.RESET + ")" + ChatColor.GREEN + " It's now time for the battle!");
+            
+            playerpair.clear();
+            
+        }
+        
+    }
+    
+    public static Battle getBattle(int index){ return currbattles.get(index); }
+    
+}
