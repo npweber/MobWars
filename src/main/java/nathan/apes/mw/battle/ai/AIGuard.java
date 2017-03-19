c3deee8ff2675909cb544d3b199b3393b1cad100
NathanApes <npweberftw@gmail.com>
2017-03-19 17:33:46 +0000

    Created "ai" classes.

---
+++ src/main/java/nathan/apes/mw/battle/ai/AIGuard.java
@@ -0,0 +1,13 @@
+package nathan.apes.mw.battle.ai;
+
+import nathan.apes.mw.battle.*;
+
+public class AIGuard {
+    
+    public AIGuard(Squad s){
+        
+        
+        
+    }
+    
+}
---
+++ src/main/java/nathan/apes/mw/battle/ai/AISnipe.java
@@ -0,0 +1,13 @@
+package nathan.apes.mw.battle.ai;
+
+import nathan.apes.mw.battle.*;
+
+public class AISnipe {
+    
+    public AISnipe(Squad s){
+        
+        
+        
+    }
+    
+}
