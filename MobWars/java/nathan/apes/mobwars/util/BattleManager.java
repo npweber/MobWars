ac7254da7a74f1fd483e3de6f2902fe8e0cf6d3f
BuildTools <unconfigured@null.spigotmc.org>
2017-07-13 15:30:04 -0700

    ~Revised Game Ending System   -No more kick msgs   -Sends to lobby

--- MobWars/java/nathan/apes/mobwars/util/BattleManager.java
+++ MobWars/java/nathan/apes/mobwars/util/BattleManager.java
@@ -12,10 +12,10 @@ import java.util.*;
 import nathan.apes.mobwars.world.battle.FindBattleground;
 import org.bukkit.*;
 import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.java.JavaPlugin;
 
-import static nathan.apes.mobwars.main.MobWars.loggingPrefix;
-import static nathan.apes.mobwars.main.MobWars.scheduler;
+import static nathan.apes.mobwars.main.MobWars.*;
 
 //The Battle Manager: Manages and Creates all on-going Battles
 
@@ -40,7 +40,22 @@ public class BattleManager {
         scheduler.scheduleSyncRepeatingTask(mainClass, () -> matchMaking(), 0L, 40L);
 
         //Update Movements
-        scheduler.scheduleSyncRepeatingTask(mainClass, () -> currbattles.forEach(battle -> Battle.getSquads(currbattles.indexOf(battle)).forEach(squad -> { squad.move(currbattles.indexOf(battle), Battle.getSquadIndex(currbattles.indexOf(battle), squad)); Squad.setSquadLocation(Squad.getSquadPlayers(currbattles.indexOf(battle), Battle.getSquadIndex(currbattles.indexOf(battle), squad)).get(0).getLocation(), currbattles.indexOf(battle), Battle.getSquadIndex(currbattles.indexOf(battle), squad)); })), 0L, 12L);
+        scheduler.scheduleSyncRepeatingTask(mainClass, () -> currbattles.forEach(
+            battle ->
+                Battle.getSquads(currbattles.indexOf(battle)).forEach(
+                    squad -> {
+                        squad.move(currbattles.indexOf(battle), Battle.getSquadIndex(currbattles.indexOf(battle), squad));
+                        Squad.setSquadLocation(
+                            Squad.getSquadPlayers(currbattles.indexOf(battle),
+                            Battle.getSquadIndex(currbattles.indexOf(battle), squad)).get(0).getLocation(),
+                                currbattles.indexOf(battle), Battle.getSquadIndex(currbattles.indexOf(battle)
+                                , squad
+                            )
+                        );
+                    }
+                )
+            ),
+        0L, 12L);
 
         //Update Game Ending
         scheduler.scheduleSyncRepeatingTask(mainClass,
@@ -49,31 +64,125 @@ public class BattleManager {
                     squad -> {
                         if(Squad.getHealth(BattleManager.getBattleIndex(battle), Battle.getSquadIndex(BattleManager.getBattleIndex(battle), squad)) < 0.5) {
                             if(!(Squad.getInForm(BattleManager.getBattleIndex(battle), Battle.getSquadIndex(BattleManager.getBattleIndex(battle), squad)))) {
-                                Battle.getBattlePlayers(BattleManager.getBattleIndex(battle)).forEach(player -> player.sendMessage(loggingPrefix + ChatColor.RED + "A Squad of Army " + ChatColor.GOLD + (Squad.getArmyIndex(BattleManager.getBattleIndex(battle), Battle.getSquadIndex(BattleManager.getBattleIndex(battle), squad)) + 1) + ChatColor.RED + " has fallen!"));
-                                Squad.getSquadPlayers(BattleManager.getBattleIndex(battle), Battle.getSquadIndex(BattleManager.getBattleIndex(battle), squad)).forEach(player -> player.sendMessage(ChatColor.RED + " Your squad has died in action!"));
+                                if(!disabling) {
+                                    Battle.getBattlePlayers(BattleManager.getBattleIndex(battle)).forEach(
+                                        player ->
+                                            player.sendMessage(loggingPrefix + ChatColor.RED + "A Squad of Army " + ChatColor.GOLD +
+                                                (Squad.getArmyIndex(BattleManager.getBattleIndex(battle), Battle.getSquadIndex(BattleManager.getBattleIndex(battle), squad)) + 1)
+                                                + ChatColor.RED + " has fallen!"
+                                            )
+                                    );
+                                    Squad.getSquadPlayers(BattleManager.getBattleIndex(battle), Battle.getSquadIndex(BattleManager.getBattleIndex(battle), squad)).forEach(
+                                        player -> player.sendMessage(ChatColor.RED + " Your squad has died in action!"));
+                                }
                             }
                             Squad.setInForm(true, BattleManager.getBattleIndex(battle), Battle.getSquadIndex(BattleManager.getBattleIndex(battle), squad));
                             Squad.setDisabled(BattleManager.getBattleIndex(battle), Battle.getSquadIndex(BattleManager.getBattleIndex(battle), squad), true);
-                            if((Squad.getDisableds(BattleManager.getBattleIndex(battle)).get(0).equals(Boolean.TRUE) && Squad.getDisableds(BattleManager.getBattleIndex(battle)).get(1).equals(Boolean.TRUE))
-                            || (Squad.getDisableds(BattleManager.getBattleIndex(battle)).get(2).equals(Boolean.TRUE) && Squad.getDisableds(BattleManager.getBattleIndex(battle)).get(3).equals(Boolean.TRUE))) {
-                                Battle.getBattlePlayers(BattleManager.getBattleIndex(battle)).forEach(player -> { player.setAllowFlight(false); player.setInvulnerable(false); });
-                                String[] kickMessages = new String[]{ "The Battle is LOST.", "You WON the Battle!" };
-                                if(Squad.getDisableds(BattleManager.getBattleIndex(battle)).get(0).equals(Boolean.TRUE) && Squad.getDisableds(BattleManager.getBattleIndex(battle)).get(1).equals(Boolean.TRUE)) {
-                                    Battle.getCommanders(BattleManager.getBattleIndex(battle))[0].kickPlayer(kickMessages[0]);
-                                    Battle.getCommanders(BattleManager.getBattleIndex(battle))[1].kickPlayer(kickMessages[1]);
-                                    Squad.getSquadPlayers(BattleManager.getBattleIndex(battle), 0).forEach(player -> player.kickPlayer(kickMessages[0]));
-                                    Squad.getSquadPlayers(BattleManager.getBattleIndex(battle), 1).forEach(player -> player.kickPlayer(kickMessages[0]));
-                                    Squad.getSquadPlayers(BattleManager.getBattleIndex(battle), 2).forEach(player -> player.kickPlayer(kickMessages[1]));
-                                    Squad.getSquadPlayers(BattleManager.getBattleIndex(battle), 3).forEach(player -> player.kickPlayer(kickMessages[1]));
-                                }
-                                if(Squad.getDisableds(BattleManager.getBattleIndex(battle)).get(2).equals(Boolean.TRUE) && Squad.getDisableds(BattleManager.getBattleIndex(battle)).get(3).equals(Boolean.TRUE)) {
-                                    Battle.getCommanders(BattleManager.getBattleIndex(battle))[0].kickPlayer(kickMessages[1]);
-                                    Battle.getCommanders(BattleManager.getBattleIndex(battle))[1].kickPlayer(kickMessages[0]);
-                                    Squad.getSquadPlayers(BattleManager.getBattleIndex(battle), 0).forEach(player -> player.kickPlayer(kickMessages[1]));
-                                    Squad.getSquadPlayers(BattleManager.getBattleIndex(battle), 1).forEach(player -> player.kickPlayer(kickMessages[1]));
-                                    Squad.getSquadPlayers(BattleManager.getBattleIndex(battle), 2).forEach(player -> player.kickPlayer(kickMessages[0]));
-                                    Squad.getSquadPlayers(BattleManager.getBattleIndex(battle), 3).forEach(player -> player.kickPlayer(kickMessages[0]));
-                                }
+                            if((Squad.getDisableds(BattleManager.getBattleIndex(battle)).get(0).equals(Boolean.TRUE)
+                                && Squad.getDisableds(BattleManager.getBattleIndex(battle)).get(1).equals(Boolean.TRUE))
+                            || (Squad.getDisableds(BattleManager.getBattleIndex(battle)).get(2).equals(Boolean.TRUE)
+                                && Squad.getDisableds(BattleManager.getBattleIndex(battle)).get(3).equals(Boolean.TRUE))) {
+                                    Battle.getBattlePlayers(BattleManager.getBattleIndex(battle)).forEach(
+                                        player -> {
+                                            player.setAllowFlight(false);
+                                            player.setInvulnerable(false);
+                                            player.getInventory().clear();
+                                            player.getInventory().setArmorContents(new ItemStack[]{});
+                                            if(config.isSet("lobbyspawnX") && config.isSet("lobbyspawnY") && config.isSet("lobbyspawnZ"))
+                                                player.teleport(new Location(Bukkit.getWorld("mw_Lobby"), config.getDouble("lobbyspawnX"),
+                                                    config.getDouble("lobbyspawnY"), config.getDouble("lobbyspawnZ")
+                                                ));
+                                            else {
+                                                mainClass.getLogger().severe("Could not end game properly. Operator forgot to set the Lobby Cords. Cannot send users back...");
+                                                player.sendMessage(ChatColor.RED + "Please contact the Operator. Your match has failed to return you to the lobby.");
+                                            }
+                                        }
+                                    );
+                                    String[] endMessages = new String[]{
+                                        loggingPrefix + ChatColor.DARK_RED + "The Battle is LOST.",
+                                        loggingPrefix + ChatColor.GREEN + "You WON the Battle!",
+                                        loggingPrefix + ChatColor.BLUE + "Your game was stopped due to the plugin being disabled."
+                                    };
+                                    if(!disabling){
+                                        if (Squad.getDisableds(BattleManager.getBattleIndex(battle)).get(0).equals(Boolean.TRUE)
+                                                && Squad.getDisableds(BattleManager.getBattleIndex(battle)).get(1).equals(Boolean.TRUE)) {
+                                            Battle.getCommanders(BattleManager.getBattleIndex(battle))[0].sendMessage(endMessages[0]);
+                                            Battle.getCommanders(BattleManager.getBattleIndex(battle))[0].playSound(
+                                                Battle.getCommanders(BattleManager.getBattleIndex(battle))[0].getLocation(),
+                                                Sound.ENTITY_GHAST_DEATH, 1.0f, 1.0f
+                                            );
+                                            Battle.getCommanders(BattleManager.getBattleIndex(battle))[1].sendMessage(endMessages[1]);
+                                            Battle.getCommanders(BattleManager.getBattleIndex(battle))[1].playSound(
+                                                Battle.getCommanders(BattleManager.getBattleIndex(battle))[1].getLocation(),
+                                                Sound.ENTITY_FIREWORK_TWINKLE, 1.0f, 1.0f
+                                            );
+                                            Squad.getSquadPlayers(BattleManager.getBattleIndex(battle), 0).forEach(
+                                                player -> {
+                                                    player.sendMessage(endMessages[0]);
+                                                    player.playSound(player.getLocation(), Sound.ENTITY_GHAST_DEATH, 1.0f, 1.0f);
+                                                }
+                                            );
+                                            Squad.getSquadPlayers(BattleManager.getBattleIndex(battle), 1).forEach(
+                                                player -> {
+                                                    player.sendMessage(endMessages[0]);
+                                                    player.playSound(player.getLocation(), Sound.ENTITY_GHAST_DEATH, 1.0f, 1.0f);
+                                                }
+                                            );
+                                            Squad.getSquadPlayers(BattleManager.getBattleIndex(battle), 2).forEach(
+                                                player -> {
+                                                    player.sendMessage(endMessages[1]);
+                                                    player.playSound(player.getLocation(), Sound.ENTITY_FIREWORK_TWINKLE, 1.0f, 1.0f);
+                                                }
+                                            );
+                                            Squad.getSquadPlayers(BattleManager.getBattleIndex(battle), 3).forEach(
+                                                player -> {
+                                                    player.sendMessage(endMessages[1]);
+                                                    player.playSound(player.getLocation(), Sound.ENTITY_FIREWORK_TWINKLE, 1.0f, 1.0f);
+                                                }
+                                            );
+                                        }
+                                        if (Squad.getDisableds(BattleManager.getBattleIndex(battle)).get(2).equals(Boolean.TRUE)
+                                                && Squad.getDisableds(BattleManager.getBattleIndex(battle)).get(3).equals(Boolean.TRUE)) {
+                                            Battle.getCommanders(BattleManager.getBattleIndex(battle))[0].sendMessage(endMessages[1]);
+                                            Battle.getCommanders(BattleManager.getBattleIndex(battle))[0].playSound(
+                                                Battle.getCommanders(BattleManager.getBattleIndex(battle))[0].getLocation(),
+                                                Sound.ENTITY_FIREWORK_TWINKLE, 1.0f, 1.0f
+                                            );
+                                            Battle.getCommanders(BattleManager.getBattleIndex(battle))[1].sendMessage(endMessages[0]);
+                                            Battle.getCommanders(BattleManager.getBattleIndex(battle))[1].playSound(
+                                                Battle.getCommanders(BattleManager.getBattleIndex(battle))[1].getLocation(),
+                                                Sound.ENTITY_GHAST_DEATH, 1.0f, 1.0f
+                                            );
+                                            Squad.getSquadPlayers(BattleManager.getBattleIndex(battle), 0).forEach(
+                                                player -> {
+                                                    player.sendMessage(endMessages[1]);
+                                                    player.playSound(player.getLocation(), Sound.ENTITY_FIREWORK_TWINKLE, 1.0f, 1.0f);
+                                                }
+                                            );
+                                            Squad.getSquadPlayers(BattleManager.getBattleIndex(battle), 1).forEach(
+                                                player -> {
+                                                    player.sendMessage(endMessages[1]);
+                                                    player.playSound(player.getLocation(), Sound.ENTITY_FIREWORK_TWINKLE, 1.0f, 1.0f);
+                                                }
+                                            );
+                                            Squad.getSquadPlayers(BattleManager.getBattleIndex(battle), 2).forEach(
+                                                player -> {
+                                                    player.sendMessage(endMessages[0]);
+                                                    player.playSound(player.getLocation(), Sound.ENTITY_GHAST_DEATH, 1.0f, 1.0f);
+                                                }
+                                            );
+                                            Squad.getSquadPlayers(BattleManager.getBattleIndex(battle), 3).forEach(
+                                                player -> {
+                                                    player.sendMessage(endMessages[0]);
+                                                    player.playSound(player.getLocation(), Sound.ENTITY_GHAST_DEATH, 1.0f, 1.0f);
+                                                }
+                                            );
+                                        }
+                                    }
+                                    if(disabling && !disabled){
+                                        Battle.getBattlePlayers(BattleManager.getBattleIndex(battle)).forEach(player -> player.sendMessage(endMessages[2]));
+                                        disabled = true;
+                                    }
                             }
                         }
                     }
@@ -82,7 +191,7 @@ public class BattleManager {
         , 0L, 40L);
         scheduler.scheduleSyncRepeatingTask(mainClass, () -> currbattles.forEach(
             battle -> {
-                if(!(Bukkit.getOnlinePlayers().containsAll(Battle.getBattlePlayers(BattleManager.getBattleIndex(battle))))) {
+                if(!(bw.getEntities().containsAll(Battle.getBattlePlayers(BattleManager.getBattleIndex(battle))))) {
                     Battle.getAllSquads().remove(BattleManager.getBattleIndex(battle));
                     Battle.getAllBattlePlayers().remove(BattleManager.getBattleIndex(battle));
                     Battle.getBattleAreas().remove(BattleManager.getBattleIndex(battle));
@@ -95,6 +204,8 @@ public class BattleManager {
                     Squad.getAllSquadLocation().remove(BattleManager.getBattleIndex(battle));
                     Squad.getAllRetreat().remove(BattleManager.getBattleIndex(battle));
                     Squad.getAllSquadPlayer().remove(BattleManager.getBattleIndex(battle));
+                    EventCommanderAction.commander1Squad.remove(BattleManager.getBattleIndex(battle));
+                    EventCommanderAction.commander2Squad.remove(BattleManager.getBattleIndex(battle));
                     BattleManager.getBattles().remove(battle);
                     battleind--;
                 }
@@ -115,7 +226,14 @@ public class BattleManager {
             battleind++;
 
             //Log MatchMake
-            battlePlayers.forEach(player -> player.sendMessage(loggingPrefix + ChatColor.GOLD + "You have a game! It's now time for the battle!"));
+            battlePlayers.forEach(
+                player -> {
+                    player.sendMessage(loggingPrefix + ChatColor.GOLD + "You have a game! It's now time for the battle!");
+                    player.sendMessage(loggingPrefix + ChatColor.AQUA + "You will now be teleported to a "
+                        + ChatColor.GOLD + "Randomly Generated Battlefield" + ChatColor.AQUA + ". Take it on with pride!"
+                    );
+                }
+            );
 
             //Misc Init
             EventCommanderAction.commander1Squad.add(null);
